f498f508dbebe380dd6e21775f94305ec2fa5ea8
Tatiana Do Nascimento Moura <tatybrigadista@hotmail.com>
2018-01-14 15:53:42 -0200

    commit

---
+++ ClassLojaVirtual/Entitades/Produtos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLojaVirtual.Entitades
+{
+    public class Produtos
+    {
+        public int ProdutoId { get; set; }
+        
+        public string Nome { get; set; }
+
+        public string Descrição { get; set; }
+
+        public decimal Preco { get; set; }
+
+
+    }
+}
