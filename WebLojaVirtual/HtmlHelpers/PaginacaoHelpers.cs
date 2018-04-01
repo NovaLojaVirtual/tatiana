9a002110506d2d26ba85c01e353b4e69a4fa90d3
Tatiana Do Nascimento Moura <tatybrigadista@hotmail.com>
2018-04-01 16:22:38 -0300

    Classes de Paginação

---
+++ WebLojaVirtual/HtmlHelpers/PaginacaoHelpers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using WebLojaVirtual.Models;
+
+namespace WebLojaVirtual.HtmlHelpers
+{
+    public static class PaginacaoHelpers
+    {
+
+        //total de paginas
+
+        public static MvcHtmlString PageLinks(this HtmlHelper html, Paginacao paginacao, Func<int, string> paginaUrl)
+        {
+
+            StringBuilder resultado = new StringBuilder();
+
+            for (int i = 1; i < paginacao.TotalPagina; i++)
+            {
+
+                TagBuilder tag = new TagBuilder("a");
+                tag.MergeAttribute("href", paginaUrl(i));
+                tag.InnerHtml = i.ToString();
+
+                if (i == paginacao.PaginaAtual)
+                {
+                    tag.AddCssClass("selected");
+                    tag.AddCssClass("btn-primary");
+
+                }
+                tag.AddCssClass("btn  btn-default");
+                resultado.Append(tag);
+            }
+            return MvcHtmlString.Create(resultado.ToString());
+        }
+
+    }
+}
---
+++ WebLojaVirtual/Models/Paginacao.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Web;
+
+namespace WebLojaVirtual.Models
+{
+    public class Paginacao
+    {
+        public  int ItensTotal { get; set; }
+
+        public  int ItensPorPagina { get; set; }
+
+        public int PaginaAtual { get; set; }
+
+        public int TotalPagina
+        {
+
+        get{ return (int)Math.Ceiling((decimal)ItensTotal / ItensPorPagina);}
+            }
+        }    
+    }
